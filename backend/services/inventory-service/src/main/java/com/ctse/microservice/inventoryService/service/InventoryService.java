cd8946584d681aaaaf10669a4d8d366cd0d349bf
Dilan Shanuka <dilanshanuka999@gmail.com>
2025-04-18 00:23:57 +0530

    Add Some methods

--- backend/services/inventory-service/src/main/java/com/ctse/microservice/inventoryService/service/InventoryService.java
+++ backend/services/inventory-service/src/main/java/com/ctse/microservice/inventoryService/service/InventoryService.java
@@ -1,8 +1,13 @@
 package com.ctse.microservice.inventoryService.service;
 
+import com.ctse.microservice.inventoryService.model.Inventory;
 import com.ctse.microservice.inventoryService.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -11,5 +16,29 @@ public class InventoryService {
 
     public boolean isInStock(String skuCode, Integer quantity) {
         return inventoryRepository.existsBySkuCodeAndQuantityGreaterThanEqual(skuCode, quantity);
+    }
+
+    public List<Inventory> getAllInventory() {
+        return inventoryRepository.findAll();
+    }
+
+    public Inventory addInventory(Inventory inventory) {
+        return inventoryRepository.save(inventory);
+    }
+
+    public Inventory updateQuantity(Long id, Integer quantity) {
+        Inventory inventory = inventoryRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Item not found"));
+        inventory.setQuantity(quantity);
+        return inventoryRepository.save(inventory);
+    }
+
+    public void deleteInventory(Long id) {
+        inventoryRepository.deleteById(id);
+    }
+
+    public Inventory getBySkuCode(String skuCode) {
+        return inventoryRepository.findBySkuCode(skuCode)
+                .orElseThrow(() -> new RuntimeException("Item not found"));
     }
 }
