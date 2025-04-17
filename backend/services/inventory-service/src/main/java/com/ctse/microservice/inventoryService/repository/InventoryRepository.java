ee957f29d721e5308e643e9bce5155228b3bd8a3
Dilan Shanuka <dilanshanuka999@gmail.com>
2025-04-18 00:33:09 +0530

    Add Some Methods

--- backend/services/inventory-service/src/main/java/com/ctse/microservice/inventoryService/repository/InventoryRepository.java
+++ backend/services/inventory-service/src/main/java/com/ctse/microservice/inventoryService/repository/InventoryRepository.java
@@ -4,7 +4,9 @@ import com.ctse.microservice.inventoryService.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
     boolean existsBySkuCodeAndQuantityGreaterThanEqual(String skuCode, Integer quantity);
+    Optional<Inventory> findBySkuCode(String skuCode);
 }
